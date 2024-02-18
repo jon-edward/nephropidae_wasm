e643136cb564bb5f4328d05cf5ed8531488bd737
jon-edward <arithmatlic@gmail.com>
2024-02-18 09:13:15 -0500

    Update

--- build.py
+++ build.py
@@ -3,7 +3,41 @@ from pathlib import Path
 import shlex
 import shutil
 import subprocess
-from typing import List
+from typing import Iterable, Union, Optional, List
+
+
+class ShellCommand:
+    parts: Iterable[Union[str, Path]]
+    cwd: Optional[Path]
+
+    def __init__(self, *parts: Union[str, Path], cwd: Optional[Path] = None):
+        self.parts = parts
+        self.cwd = cwd
+    
+    @property
+    def parts_str(self) -> str:
+        return shlex.join([str(part) for part in self.parts])
+    
+    @property
+    def cwd_str(self) -> Optional[str]:
+        return None if not self.cwd or self.cwd == Path.cwd() else str(self.cwd)
+
+    
+    def __str__(self):
+        parts_str = self.parts_str
+        cwd_str = self.cwd_str
+
+        if self.cwd:
+            return f"(cd {cwd_str} && {parts_str})"
+        
+        return parts_str
+    
+    def show(self):
+        print(str(self))
+    
+    def run(self):
+        self.show()
+        subprocess.run(self.parts_str, cwd=self.cwd_str, shell=True)
 
 
 def main(
@@ -15,31 +49,29 @@ def main(
     no_run: bool,
 ) -> List[str]:
 
-    lobster_executable = str(lobster_root.joinpath("bin", "lobster"))
+    lobster_executable = lobster_root.joinpath("bin", "lobster")
     wasm_root = lobster_root.joinpath("dev", "emscripten")
 
-    compile_cpp_command = shlex.join([lobster_executable, "--cpp", str(entry)])
+    commands = [
+        ShellCommand(lobster_executable, "--cpp", entry),
+        ShellCommand("make", "-j8", cwd=wasm_root),
+        ShellCommand("cp", "-r", wasm_root, out_dir),
+        ShellCommand("cp", html, out_dir.joinpath("index.html")),
+    ]
 
-    make_command = shlex.join(["make", "-j8"])
-
-    copy_html_command = shlex.join(
-        ["cp", str(html), str(out_dir.joinpath("index.html"))]
-    )
-
-    copy_emscripten_command = shlex.join(["cp", "-r", str(wasm_root), str(out_dir)])
-
-    serve_command = shlex.join(
-        [
-            "python3",
-            "-m",
-            "http.server",
-            "8080",
-            "--bind",
-            "127.0.0.1",
-            "--directory",
-            str(out_dir),
-        ]
-    )
+    if serve:
+        commands.append(
+            ShellCommand(
+                "python3",
+                "-m",
+                "http.server",
+                "8080",
+                "--bind",
+                "127.0.0.1",
+                "--directory",
+                out_dir
+            )
+        )
 
     if not no_run:
         print()
@@ -49,17 +81,14 @@ def main(
         except FileNotFoundError:
             pass
 
-        subprocess.run(compile_cpp_command, shell=True)
-        subprocess.run(make_command, shell=True, cwd=wasm_root)
-        subprocess.run(copy_emscripten_command, shell=True)
-        subprocess.run(copy_html_command, shell=True)
-        
-        print("Finished building.\n")
+        for command in commands:
+            command.run()
+    
+    else:
+        for command in commands:
+            command.show()
 
-        if serve:
-            subprocess.run(serve_command, shell=True)
-
-    return []
+    return "\n".join(str(command) for command in commands)
 
 
 if __name__ == "__main__":
